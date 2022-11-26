6522415f636dcf26a7f99b372b3d1c9d0fb764ff
Amber <amber.nowakowski@gmail.com>
2022-11-26 15:28:07 -0600

    allow user to deregister from classes

--- server.js
+++ server.js
@@ -130,6 +130,50 @@ app.post("/class/register", async (req, res) => {
   }
 });
 
+app.post("/class/deregister", async (req, res) => {
+  try {
+    let data = req.body;
+    let classId = data.classId;
+    let userToken = req.body.token;
+    // validate user token
+    // see which user belongs to this token
+    let auth = await Auth.findOne({ token: userToken });
+
+    let userId = auth.userId;
+    if (!userId) throw new Error("token is invalid");
+
+    let userInDatabase = await User.findOne({ _id: userId });
+    if (!userInDatabase) throw new Error("user does not exist");
+    // get yoga class
+    let yogaClass = await Class.findOne({ _id: classId });
+    // establish rules
+
+    if (yogaClass.registeredUsers.indexOf(userId) == -1)
+      throw new Error("user is not registered in this class");
+
+    yogaClass.registeredUsers = yogaClass.registeredUsers.filter(
+      (student) => student !== userId
+    );
+    yogaClass.capacity += 1;
+    await yogaClass.save();
+
+    if (!userInDatabase.registeredClasses) {
+      userInDatabase.registeredClasses = [];
+    }
+
+    let updatedClasses = userInDatabase.registeredClasses.filter(
+      (yoga) => yoga !== classId
+    );
+
+    userInDatabase.registeredClasses = updatedClasses;
+    userInDatabase.save();
+
+    return res.json({ success: true });
+  } catch (e) {
+    return res.json({ success: false, message: e.message });
+  }
+});
+
 app.get("/classes", async (req, res) => {
   let start = req.query.start; // timestamp
   let days = req.query.days; // days
